aaa1a57a848cef1204f7dfb372e2e1bac07d9fc4
toshitana <roll2361good@gmail.com>
2023-11-05 22:07:39 +0900

    first commit

---
+++ st_helio.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import sys
+
+#if sys.platform == 'win32':
+#    import asyncio
+#    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+st.write("Hello world. Let's learn how to build a AI-based app together.")
---
+++ up.py
@@ -0,0 +1,22 @@
+# pip install PyPDF2
+from PyPDF2 import PdfReader
+
+def get_pdf_text():
+    uploaded_file = st.file_uploader(
+        label='Upload your PDF here😇',
+        type='pdf'  # アップロードを許可する拡張子 (複数設定可)
+    )
+    if uploaded_file:
+        pdf_reader = PdfReader(uploaded_file)
+        text = '\n\n'.join([page.extract_text() for page in pdf_reader.pages])
+        text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+            model_name=st.session_state.emb_model_name,
+            # 適切な chunk size は質問対象のPDFによって変わるため調整が必要
+            # 大きくしすぎると質問回答時に色々な箇所の情報を参照することができない
+            # 逆に小さすぎると一つのchunkに十分なサイズの文脈が入らない
+            chunk_size=250,
+            chunk_overlap=0,
+        )
+        return text_splitter.split_text(text)
+    else:
+        return None
